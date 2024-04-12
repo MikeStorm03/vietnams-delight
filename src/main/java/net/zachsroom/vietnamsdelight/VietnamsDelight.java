c02a971f931e2d197c730427c5f982ad1797b886
ZachsRoom <therealvnplayer@gmail.com>
2024-04-12 22:03:36 +0700

    Initial commit

---
+++ src/main/java/net/zachsroom/vietnamsdelight/VietnamsDelight.java
@@ -0,0 +1,16 @@
+package net.zachsroom.vietnamsdelight;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VietnamsDelight implements ModInitializer {
+	public static final String MOD_ID = "vietnamsdelight";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/zachsroom/vietnamsdelight/VietnamsDelightClient.java
@@ -0,0 +1,10 @@
+package net.zachsroom.vietnamsdelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VietnamsDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
